253facb4f32845fef0c15309e5a9c22eb27ac62f
Johannes Chorzempa <jchorzempa@posteo.org>
2023-05-04 15:57:02 +0200

    Add loggedin & out logic to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,27 +1,42 @@
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../context/auth.context';
+import { useContext } from 'react';
 
 function NavBar() {
+	const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
+
 	return (
 		// Temporary NavBar for easier navigation during the initial development phase. To be improved later.
-		<div>
-			<Link to="/"><Button variant='text'>Browse</Button></Link>
-			<Link to="/profile">
-        <Button variant='text'>
-          Profile
+
+		<nav>
+      {!isLoggedIn && (
+
+          <>
+        <Link to='/login'>
+          <Button variant='text'>Log In</Button>
+        </Link>
+        <Link to='/signup'>
+          <Button variant='text'>Sign Up</Button>
+        </Link>
+          </> )}
+
+          {isLoggedIn && (
+
+          <>
+        <Link to='/'>
+          <Button variant='text'>Browse</Button>
+        </Link>
+        <Link to='/profile'>
+          <Button variant='text'>Profile</Button>
+        </Link>
+
+        <Button variant='text' onClick={logOutUser}>
+          Log Out
         </Button>
-      </Link>
-			<Link to="/login">
-        <Button variant='text'>
-          Log In
-        </Button>
-      </Link>
-			<Link to="/register">
-        <Button variant='text'>
-          Register
-        </Button>
-      </Link>
-		</div>
+
+      </> )}
+    </nav>
 	);
 }
 
