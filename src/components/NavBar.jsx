142a2dbcd347978d2f46bf30928828637879513f
Nana Chuto <mail@nanachuto.com>
2023-05-04 16:26:50 +0200

    Quality of life logo addition to NavBar loll

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,43 +1,45 @@
-import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom';
-import { AuthContext } from '../context/auth.context';
-import { useContext } from 'react';
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../context/auth.context";
+import { useContext } from "react";
 
 function NavBar() {
-	const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
+  const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
 
-	return (
-		// Temporary NavBar for easier navigation during the initial development phase. To be improved later.
+  return (
+    // Temporary NavBar for easier navigation during the initial development phase. To be improved later.
 
-		<nav>
+    <nav>
+      <Link to="/">
+        <h2>USEUM</h2>
+      </Link>
       {!isLoggedIn && (
+        <>
+          <Link to="/login">
+            <Button variant="text">Log In</Button>
+          </Link>
+          <Link to="/signup">
+            <Button variant="text">Sign Up</Button>
+          </Link>
+        </>
+      )}
 
-          <>
-        <Link to='/login'>
-          <Button variant='text'>Log In</Button>
-        </Link>
-        <Link to='/signup'>
-          <Button variant='text'>Sign Up</Button>
-        </Link>
-          </> )}
+      {isLoggedIn && (
+        <>
+          <Link to="/">
+            <Button variant="text">Browse</Button>
+          </Link>
+          <Link to="/profile">
+            <Button variant="text">Profile</Button>
+          </Link>
 
-          {isLoggedIn && (
-
-          <>
-        <Link to='/'>
-          <Button variant='text'>Browse</Button>
-        </Link>
-        <Link to='/profile'>
-          <Button variant='text'>Profile</Button>
-        </Link>
-
-        <Button variant='text' onClick={logOutUser}>
-          Log Out
-        </Button>
-
-      </> )}
+          <Button variant="text" onClick={logOutUser}>
+            Log Out
+          </Button>
+        </>
+      )}
     </nav>
-	);
+  );
 }
 
 export default NavBar;
