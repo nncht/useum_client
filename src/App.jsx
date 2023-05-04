5367728e767ce4966ebe6d1dfdccee6a8d50e72a
Nana Chuto <mail@nanachuto.com>
2023-05-04 11:56:08 +0200

    Renamed LandingPage.jsx to Home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
-import { useState } from 'react'
-import './App.css'
-import LandingPage from './pages/LandingPage'
-import { Routes, Route } from 'react-router-dom';
-
+import { useState } from "react";
+import "./App.css";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
-
-      <div className='App'>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-        </Routes>
-      </div>
-
-  )
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
