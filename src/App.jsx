6cc6dcf945967c3f113063495393e8e7b7c38d53
Nana Chuto <mail@nanachuto.com>
2023-05-04 12:47:41 +0200

    Basic NavBar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
 import "./App.css";
 import Home from "./pages/Home";
+import NavBar from "./components/NavBar";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    // Temporary NavBar for easier navigation during the initial development phase. To be improved later.
+    <div>
+      <button>
+        <a href="/">Browse</a>
+      </button>
+      <button>
+        <a href="/profile">Profile</a>
+      </button>
+      <button>
+        <a href="/login">Log In</a>
+      </button>
+      <button>
+        <a href="/register">Sign Up</a>
+      </button>
+    </div>
+  );
+}
+
+export default NavBar;
