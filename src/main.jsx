ecbf222535352e46fe45b8fbb8f110ae5162a521
Johannes Chorzempa <jchorzempa@posteo.org>
2023-05-04 15:56:22 +0200

    wrap the authprovider around app

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router} from 'react-router-dom'
+import { AuthProvider } from './context/auth.context.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </Router>
   </React.StrictMode>,
 )
