cc1783a5296277bb62e81f93342c05009aa6d090
Nana Chuto <mail@nanachuto.com>
2023-05-04 21:55:08 +0200

    Cleaned up Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,43 +1,15 @@
 import { AuthContext } from "../context/auth.context";
-import { useContext, useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-
-
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+  const { user, isLoggedIn } = useContext(AuthContext);
 
-  const {user, isLoggedIn} = useContext(AuthContext);
-
-  return (
-
-    !isLoggedIn ? <h1>Not logged in</h1> :
+  return !isLoggedIn ? (
+    <h1>Not logged in</h1>
+  ) : (
     <div>
-      <h1>Welcome to the Landing Page!</h1>
-
-      <br />
-      <br />
-
-      <h3>
-        You can already see ALMOST everything there is to see here, except for:
-      </h3>
-
-      <br />
-
-      <ol>
-        <li>the user dashboard</li>
-        <li>personalized recommendations</li>
-        <li>the last item you liked</li>
-        <li>the create collection form</li>
-        <li>the create item form</li>
-        <li>and so on</li>
-        <a href="#">test link</a>
-      </ol>
-      <br />
-
-      <h3>
-        What you CAN see here already is the Navbar up there that will permit
-        you to log in or sign up!
-      </h3>
+      <h3>Welcome back, User</h3>
     </div>
   );
 };
