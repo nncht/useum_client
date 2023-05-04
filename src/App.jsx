03e0ba5a54499b85c193250eaec222229ed413b7
Johannes Chorzempa <jchorzempa@posteo.org>
2023-05-04 15:56:45 +0200

    add login and signup routes to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import NavBar from "./components/NavBar";
-import { Routes, Route } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
   return (
@@ -10,6 +12,9 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+
       </Routes>
     </div>
   );
