bb4cfc7801b99164551db98315fa44a4ef4ff0fb
Lukas Merlin Jürgens <108548830+lukasmerlin@users.noreply.github.com>
2023-05-04 21:08:25 +0200

    Create Item Routes/Component/Page
    - CreateItem Component
    - CreateItem Page
    - CreateItem Routes

---
+++ src/pages/CreateItemPage.jsx
@@ -0,0 +1,12 @@
+import CreateItemForm from '../components/CreateItemForm';
+
+function CreateItemPage() {
+  return (
+    <div>
+      <h1>Create Item</h1>
+      <CreateItemForm />
+    </div>
+  );
+}
+
+export default CreateItemPage;
