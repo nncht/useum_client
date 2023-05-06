029ed5ed9833273332f42a6c68797e185eb3398d
Nana Chuto <mail@nanachuto.com>
2023-05-06 15:07:39 +0200

    Add default header image

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -2,19 +2,8 @@ import React from "react";
 
 const ProfileHeader = () => {
   return (
-    <div className="top">
-      Hi
-      {/* <button onClick={navigateBack}>
-        <ArrowLeft size={20} color="white" />
-      </button>
-      <div className="info">
-        <h1>{user.data.name}</h1>
-        <span className="info__tweets-count">{activitiesCount} Tweets</span>
-      </div>
-
-      <div className="cover">
-        <img src="https://picsum.photos/500/300" />
-      </div> */}
+    <div className="cover">
+      <img src="images/default/default-header.png" />
     </div>
   );
 };
