5b81b7ce5889295c05e717ef6329f1394847716c
Nana Chuto <mail@nanachuto.com>
2023-05-06 18:11:19 +0200

    Further positioning

--- src/components/Profile/ProfileBio.jsx
+++ src/components/Profile/ProfileBio.jsx
@@ -1,17 +1,11 @@
-import React from "react";
-
 const ProfileBio = ({ currentUser }) => {
   return (
-    <div>
-      <div className="bg-slate-600 px-3 py-2 h-30">
-        <h2 className="text-3xl text-white">{currentUser.username}</h2>
-        <p className="text-white">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat.
-        </p>
-      </div>
+    <div className="bg-slate-600 px-3 py-2 h-30 min-h-200">
+      <h2 className="text-3xl text-white">{currentUser.username}</h2>
+      <p className="text-white">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
     </div>
   );
 };
