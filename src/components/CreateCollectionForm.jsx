5b7511b4505bae34ba9bf614f4349705fd87cb30
Johannes Chorzempa <jchorzempa@posteo.org>
2023-05-06 16:03:23 +0200

    Update create collection form component

--- src/components/CreateCollectionForm.jsx
+++ src/components/CreateCollectionForm.jsx
@@ -1,45 +1,75 @@
-import { useState } from 'react';
-import PropTypes from 'prop-types';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../context/auth.context';
 
-function CreateCollectionForm({ onCreateCollection }) {
-  const [name, setName] = useState('');
+const API_URL = 'http://localhost:5005';
+function CreateCollectionForm() {
+	const { user } = useContext(AuthContext);
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-  
-    try {
-      const response = await fetch('http://localhost:5005/collections', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ name }),
-      });
-  
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-  
-      onCreateCollection(name);
-      setName('');
-    } catch (error) {
-      console.error('Error creating collection:', error);
-    }
-  };
+	const [currentUser, setCurrentUser] = useState(user);
+	const [name, setName] = useState('');
+	const [description, setDescription] = useState('');
+	const storedToken = localStorage.getItem('authToken');
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input type="text" value={name} onChange={(event) => setName(event.target.value)} />
-      </label>
-      <button type="submit">Create</button>
-    </form>
-  );
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		if (user && user._id) {
+			axios
+				.get(`${API_URL}/users/${user._id}`)
+				.then((res) => {
+					setCurrentUser(res.data);
+				})
+				.catch((err) => {
+					console.error(err);
+				});
+		}
+	}, [user]);
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+
+		const params = { name: name, description: description, createdBy: user._id };
+
+		axios
+			.post(`${API_URL}/collections`, params, {
+				headers: { Authorization: `Bearer ${storedToken}` },
+			})
+			.then((res) => {
+        console.log(res.data);
+				setName('');
+				setDescription('');
+				navigate('/collections');
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	};
+
+	return (
+		currentUser && (
+			<form onSubmit={handleSubmit}>
+				<label htmlFor="name">
+					Name:
+					<input type='text' id="name" value={name} onChange={(event) => setName(event.target.value)} />
+				</label>
+
+				<input type='hidden' name='' value={currentUser._id} />
+
+				<label htmlFor="description">
+					Description:
+					<textarea id="description" value={description} onChange={(event) => setDescription(event.target.value)} />
+
+
+				</label>
+
+
+				<button type='submit'>Create</button>
+			</form>
+		)
+	);
 }
-
-CreateCollectionForm.propTypes = {
-    onCreateCollection: PropTypes.func.isRequired,
-  };
 
 export default CreateCollectionForm;
