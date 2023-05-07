39294edabed34f7793d235faafaea90607cc18d5
Johannes Chorzempa <jchorzempa@posteo.org>
2023-05-07 13:11:32 +0200

    Add category exists condition

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,88 +1,88 @@
-import { AuthContext } from "../context/auth.context";
-import { useState, useEffect, useContext } from "react";
-import { Link, useNavigate } from "react-router-dom";
-import axios from "axios";
-import Button from "@mui/material/Button";
-import ProfileHeader from "../components/Profile/ProfileHeader";
-import ProfileBio from "../components/Profile/ProfileBio";
-import ProfilePicture from "../components/Profile/ProfilePicture";
+import { AuthContext } from '../context/auth.context';
+import { useState, useEffect, useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import Button from '@mui/material/Button';
+import ProfileHeader from '../components/Profile/ProfileHeader';
+import ProfileBio from '../components/Profile/ProfileBio';
+import ProfilePicture from '../components/Profile/ProfilePicture';
 
-const API_URL = "http://localhost:5005";
+const API_URL = 'http://localhost:5005';
 
 // DETERMINE CURRENT USER
 const ProfilePage = () => {
-  const { user } = useContext(AuthContext);
+	const { user } = useContext(AuthContext);
 
-  const [currentUser, setCurrentUser] = useState(user);
+	const [currentUser, setCurrentUser] = useState(user);
 
-  useEffect(() => {
-    if (user && user._id) {
-      axios
-        .get(`${API_URL}/users/${user._id}`)
-        .then((res) => {
-          setCurrentUser(res.data);
-          console.log(res.data);
-        })
-        .catch((err) => {
-          console.error(err);
-        });
-    }
-  }, [user]);
+	useEffect(() => {
+		if (user && user._id) {
+			axios
+				.get(`${API_URL}/users/${user._id}`)
+				.then((res) => {
+					setCurrentUser(res.data);
+					console.log(res.data);
+				})
+				.catch((err) => {
+					console.error(err);
+				});
+		}
+	}, []);
 
-  console.log("the currentUser", currentUser);
 
-  // USER PROFILE RENDER
-  return (
-    currentUser && (
-      <div>
-        {/* Header and profile picture block */}
-        <div className="relative">
-          <ProfileHeader />
-          <div className="absolute mt-[-80px] mx-4">
-            <ProfilePicture currentUser={currentUser} />
-          </div>
-        </div>
+	// USER PROFILE RENDER
 
-        {/* User bio, needs to be added to User model */}
-        <div>
-          <ProfileBio currentUser={currentUser} />
-        </div>
+	if (currentUser.categories || currentUser.collections) {
+		return (
+			<div>
+				{/* Header and profile picture block */}
+				<div className='relative'>
+					<ProfileHeader />
+					<div className='absolute mt-[-80px] mx-4'>
+						<ProfilePicture currentUser={currentUser} />
+					</div>
+				</div>
 
-        {/* TO BE MOVED TO OWN COMPONENTS */}
-        <section className="p-3 bg-slate-300">
-          <h4 className="text-2xl text-slate-600">Collections</h4>
+				{/* User bio, needs to be added to User model */}
+				<div>
+					<ProfileBio currentUser={currentUser} />
+				</div>
 
-          {!currentUser.collections ? (
-            <p>You've not added any collections.</p>
-          ) : (
-            currentUser.collections.map((collection) => {
-              return (
-                <div key={collection._id}>
-                  <Link to={`/my-collections/${collection._id}`}>
-                    <p>{collection.name}</p>
-                  </Link>
-                </div>
-              );
-            })
-          )}
+				{/* TO BE MOVED TO OWN COMPONENTS */}
+				<section className='p-3 bg-slate-300'>
+					<h4 className='text-2xl text-slate-600'>Collections</h4>
 
-          <h4 className="text-2xl text-slate-600">Interests</h4>
+					{currentUser.collections.length === 0 ? (
+						<p>You've not added any collections.</p>
+					) : (
+						currentUser.collections.map((collection) => {
+							return (
+								<div key={collection._id}>
+									<Link to={`/my-collections/${collection._id}`}>
+										<p>{collection.name}</p>
+									</Link>
+								</div>
+							);
+						})
+					)}
 
-          {!currentUser.categories ? (
-            <p>You've not specified any interests yet.</p>
-          ) : (
-            currentUser.categories.map((category) => {
-              return (
-                <div key={category._id}>
-                  <p>{category.name}</p>
-                </div>
-              );
-            })
-          )}
-        </section>
-      </div>
-    )
-  );
+					<h4 className='text-2xl text-slate-600'>Interests</h4>
+
+					{currentUser.categories.length === 0 ? (
+						<p>You've not specified any interests yet.</p>
+					) : (
+						currentUser.categories.map((category) => {
+							return (
+								<div key={category._id}>
+									<p>{category.name}</p>
+								</div>
+							);
+						})
+					)}
+				</section>
+			</div>
+		);
+	}
 };
 
 export default ProfilePage;
