b3c45f48c56771d769040458b8f6c51bd1ea9509
Johannes Chorzempa <jchorzempa@posteo.org>
2023-05-08 15:28:23 +0200

    Move getCollection function out of mycollection page

--- src/pages/CollectionPages/MyCollection.jsx
+++ src/pages/CollectionPages/MyCollection.jsx
@@ -1,54 +1,39 @@
 import { Link, useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import Button from '@mui/material/Button';
+import getCollection from '../../services/getCollection';
 
 const API_URL = 'http://localhost:5005';
 
 const MyCollection = () => {
-
-    const [collection, setCollection] = useState(null);
-    const {collectionId} = useParams();
-
-
-    const getCollection = () => {
-        const storedToken = localStorage.getItem('authToken');
-        axios.get(`${API_URL}/collections/${collectionId}`, {
-            headers: {
-                Authorization: `Bearer ${storedToken}`,
-            },
-        })
-        .then((response) => {
-            console.log(response.data);
-            setCollection(response.data);
-        })
-        .catch((error) => {
-            console.error(error);
-        });
-    };
-
-    useEffect(() => {
-        getCollection();
-    }, []);
+	const [collection, setCollection] = useState(null);
+	const { collectionId } = useParams();
 
 
-  return (
-    <>
-
-        <h1>My Collection</h1>
-
-        {collection && (
-            <>
-                <h2>{collection.name}</h2>
-                <p>{collection.description}</p>
-                <img src={collection.imageUrl} alt="collection" />
-            </>
-
-        )}
+	useEffect(() => {
+		getCollection(collectionId, setCollection);
+	}, []);
 
 
-    </>
-  )
-}
+	return (
+		<>
+			<h1>My Collection</h1>
 
-export default MyCollection
+			{collection && (
+				<>
+					<h2>{collection.name}</h2>
+					<p>{collection.description}</p>
+					<img src={collection.imageUrl} alt='collection' />
+
+                    <Link to={`/edit-collection/${collection._id}`}>
+                        <Button>Edit Collection</Button>
+                    </Link>
+
+				</>
+			)}
+		</>
+	);
+};
+
+export default MyCollection;
