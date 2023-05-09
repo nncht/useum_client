8e9a93f44b83d9c5d8c0859935ab6fa54d925752
Johannes Chorzempa <jchorzempa@posteo.org>
2023-05-09 13:27:09 +0200

    Slight alteration in the home.jsx code

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,43 +1,52 @@
-import { AuthContext } from "../context/auth.context";
-import { useState, useEffect, useContext } from "react";
-import axios from "axios";
-import AllCollections from "../components/Collections/AllCollections";
+import { AuthContext } from '../context/auth.context';
+import { useState, useEffect, useContext } from 'react';
+import axios from 'axios';
+import AllCollections from '../components/Collections/AllCollections';
 
 const Home = () => {
-  const [collections, setCollections] = useState({ collections: [] });
-  const API_URL = "http://localhost:5005";
-  const { user, isLoggedIn } = useContext(AuthContext);
+	const [collections, setCollections] = useState({ collections: [] });
+	const API_URL = 'http://localhost:5005';
+	const { user, isLoggedIn } = useContext(AuthContext);
 
-  useEffect(() => {
-    axios
-      .get(`${API_URL}/collections`)
-      .then((res) => {
-        const sortedCollections = res.data.collections.sort(
-          (a, b) => b.likes.length - a.likes.length
-        );
-        setCollections({ collections: sortedCollections });
-        console.log(res.data);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }, []);
+	useEffect(() => {
+		axios
+			.get(`${API_URL}/collections`)
+			.then((res) => {
+				const sortedCollections = res.data.collections.sort((a, b) => b.likes.length - a.likes.length);
+				setCollections({ collections: sortedCollections });
+				console.log(res.data);
+			})
+			.catch((err) => {
+				console.error(err);
+			});
+	}, []);
 
-  return !isLoggedIn ? (
-    <section id="main-content" className="p-4 bg-slate-300">
-      <p>Not logged in</p>
-      <AllCollections collections={collections} />
-    </section>
-  ) : (
-    <section id="main-content">
-      <div className="px-4 py-2 bg-slate-300">
-        <p className="text-2xl text-slate-600">Welcome back, {user.username}</p>
-      </div>
-      <div className="px-4 pb-20 bg-slate-300">
-        <AllCollections collections={collections} />
-      </div>
-    </section>
-  );
+	return (
+		<section id='main-content'>
+			{!isLoggedIn ? (
+				<div className='px-4 py-2 bg-slate-300'>
+					<p className='text-2xl text-slate-600'>(You need to log in still!)</p>
+				</div>
+			) : (
+				<div className='px-4 py-2 bg-slate-300'>
+					<p className='text-2xl text-slate-600'>Welcome back, {user.username}</p>
+          <p>Based on what you selected in your settings, you could see a bunch of stuff here!</p>
+          <p>Examples include:</p>
+          <ul>
+            <li>Recommended collections based on your interests</li>
+            <li>Recommended collections based on your friends' interests</li>
+            <li>The newest items</li>
+            <li>The most popular items</li>
+            <li>Lukas' favorite Ravelist</li>
+          </ul>
+				</div>
+			)}
+
+			<div className='px-4 pb-20 bg-slate-300'>
+				<AllCollections collections={collections} />
+			</div>
+		</section>
+	);
 };
 
 export default Home;
