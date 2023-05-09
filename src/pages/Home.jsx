29cab78d0bc8e107436bac275385285f3fb5df80
Nana Chuto <mail@nanachuto.com>
2023-05-09 04:36:51 +0200

    Fixed imports, added conditional back

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
-// import { AuthContext } from "../context/auth.context";
-import { useState, useEffect } from "react";
+import { AuthContext } from "../context/auth.context";
+import { useState, useEffect, useContext } from "react";
 import { Grid } from "@mui/material";
 import CollectionCard from "../components/Collections/CollectionCard";
 import axios from "axios";
@@ -8,13 +8,15 @@ const Home = () => {
   const [collections, setCollections] = useState({ collections: [] });
   const API_URL = "http://localhost:5005";
   // const { user } = useContext(AuthContext);
-
+  const { user, isLoggedIn } = useContext(AuthContext);
 
   useEffect(() => {
     axios
       .get(`${API_URL}/collections`)
       .then((res) => {
-        const sortedCollections = res.data.collections.sort((a, b) => b.likes.length - a.likes.length);
+        const sortedCollections = res.data.collections.sort(
+          (a, b) => b.likes.length - a.likes.length
+        );
         setCollections({ collections: sortedCollections });
         console.log(res.data);
       })
@@ -23,34 +25,46 @@ const Home = () => {
       });
   }, []);
 
-  return (
+  return !isLoggedIn ? (
+    <section id="main-content" className="p-3 bg-slate-300">
+      <p>Not logged in</p>
+    </section>
+  ) : (
     <section id="main-content" className="p-3 bg-slate-300">
       <div>
-        <p className="text-2xl text-slate-600">Welcome back, my dear Friend {/*user.username*/ /*I broke this, please help me fix it*/}</p>
+        <p className="text-2xl text-slate-600">Welcome back, {user.username}</p>
       </div>
-      <div>
-        <h4 className="text-2xl text-slate-600">Collections</h4>
-        <Grid container spacing={3}>
-          {/* Available collections of all users will be rendered as cards here */}
-          {collections.collections.length < 3 ? (
-            <p>No collections available</p>
-          ) : (
-            collections.collections.map((collection) => {
-              return (
-                <Grid item xs={12} sm={6} md={4} lg={3} key={collection._id}>
-                  <CollectionCard key={collection._id} collection={collection} />
-                </Grid>
-              );
-            })
-          )}
-        </Grid>
-      </div>
-
     </section>
-
   );
+  // <section id="main-content" className="p-3 bg-slate-300">
+  //   <div>
+  //     <p className="text-2xl text-slate-600">
+  //       Welcome back, {user.username}{" "}
+  //       {/*user.username*/
+  //       /*I broke this, please help me fix it*/}
+  //     </p>
+  //   </div>
+  //   <div>
+  //     <h4 className="text-2xl text-slate-600">Collections</h4>
+  //     <Grid container spacing={3}>
+  //       {/* Available collections of all users will be rendered as cards here */}
+  //       {collections.collections.length < 3 ? (
+  //         <p>No collections available</p>
+  //       ) : (
+  //         collections.collections.map((collection) => {
+  //           return (
+  //             <Grid item xs={12} sm={6} md={4} lg={3} key={collection._id}>
+  //               <CollectionCard
+  //                 key={collection._id}
+  //                 collection={collection}
+  //               />
+  //             </Grid>
+  //           );
+  //         })
+  //       )}
+  //     </Grid>
+  //   </div>
+  // </section>
 };
-
-
 
 export default Home;
