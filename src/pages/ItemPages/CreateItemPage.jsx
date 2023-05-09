3116afc9f1004fdc33253cd4d6564fe4de72fdee
Johannes Chorzempa <jchorzempa@posteo.org>
2023-05-09 21:38:44 +0200

    connect collectionId to item being created

--- src/pages/ItemPages/CreateItemPage.jsx
+++ src/pages/ItemPages/CreateItemPage.jsx
@@ -1,12 +1,17 @@
 import CreateItemForm from "../../components/CreateItemForm";
 import ItemList from "../../components/ItemList";
 import CreateForm from "../../components/CreateForm";
+import { getCollectionId } from '../../services/sharedDatastore';
+
 
 function CreateItemPage() {
+  const collectionId = getCollectionId();
+  console.log("CollectionID", collectionId);
+
   return (
     <div id="main-content">
       <h1>Create Item</h1>
-      <CreateForm target={"items"} idObject={"item"}/>
+      <CreateForm target={"items"} idObject={"item"} forCollection={collectionId}/>
 
     </div>
   );
