58867f70ee33589368ea829cd298948ec0f74e2f
Johannes Chorzempa <jchorzempa@posteo.org>
2023-05-10 20:04:31 +0200

    Show item categories

--- src/pages/ItemPages/MyItem.jsx
+++ src/pages/ItemPages/MyItem.jsx
@@ -2,12 +2,15 @@ import { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import axios from 'axios';
 import Button from '@mui/material/Button';
+import { setCollectionId } from '../../services/sharedDatastore';
+import { getCollectionId } from "../../services/sharedDatastore";
+
 
 const API_URL = 'http://localhost:5005';
 const MyItem = () => {
 	const { itemId } = useParams();
-
 	const [item, setItem] = useState({});
+	const collectionId = getCollectionId();
 
 	useEffect(() => {
 		axios
@@ -19,25 +22,32 @@ const MyItem = () => {
 			.catch((error) => {
 				console.log(error);
 			});
-	}, []);
+	}, [itemId]);
 
-	console.log(item.imageUrl);
+	console.log(item.categories);
 
 	return (
 		<>
-
-    {/* Displays the item details */}
+			{/* Displays the item details */}
 
 			{item && (
-
 				<div id='main-content' className='bg-slate-300'>
-        <section className='px-4 pt-3 pb-20 bg-slate-300'>
+					<section className='px-4 pt-3 pb-20 bg-slate-300'>
 						<div>
 							<img src={item.imageUrl} width={250} height={350} alt='an item' />
 							<h4 className='text-2xl text-slate-600'>{item.name}</h4>
 							<p>{item.description}</p>
 
-              {/* If there are reviews, displays the reviews for the item */}
+
+
+							{item.categories && (<div>
+								<h4 className='text-2xl text-slate-600'>Tags</h4>
+								{item.categories.map((tag) => {
+									return <p key={tag._id}>{tag.category}</p>;
+								})}
+							</div>)}
+
+							{/* If there are reviews, displays the reviews for the item */}
 
 							{item.reviews && (
 								<div>
@@ -53,7 +63,7 @@ const MyItem = () => {
 								</div>
 							)}
 						</div>
-            <Link to={`/edit-item/${item._id}`}>
+						<Link to={`/edit-item/${item._id}`} onClick={() => setCollectionId(collectionId)}>
 							<Button>Edit Item</Button>
 						</Link>
 					</section>
