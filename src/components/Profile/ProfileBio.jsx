74c2dcf1ca15332f7f4d09b484a09e2acb700d20
Nana Chuto <mail@nanachuto.com>
2023-05-11 22:03:15 +0200

    Added pronouns

--- src/components/Profile/ProfileBio.jsx
+++ src/components/Profile/ProfileBio.jsx
@@ -9,13 +9,24 @@ const ProfileBio = ({ currentUser }) => {
         <Button variant="contained">Edit Profile</Button>
       </div>
       <div>
-        <h2 className="text-3xl text-white">{currentUser.username}</h2>
+        <div className="flex">
+          <h2 className="mr-4 self-end text-3xl text-white m-0">
+            {currentUser.username}
+          </h2>
+          <i className="text-md text-white self-end p-1 ml-2">
+            {/* Should be ({currentUser.pronouns}), but waiting for updated User model and Edit profile function. Leaving placeholder for now.*/}
+            {/* ({currentUser.pronouns}) */}
+            (they/them)
+          </i>
+        </div>
+
         <div className="my-2">
           <UserStatistics currentUser={currentUser} />
         </div>
         <p className="text-white text-lg">
           {/* Needs to be replaced with {currentUser.userBio} once the Edit Profile function is ready.
           Leaving Lorem ipsum for testing purposes for now. */}
+          {/* {currentUser.userBio} */}
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </p>
