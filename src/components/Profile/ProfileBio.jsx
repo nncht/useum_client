d1855f4b595c36511945142fd48c0ea220d2d79c
Nana Chuto <mail@nanachuto.com>
2023-05-11 21:17:49 +0200

    Create UserStatistics.jsx

--- src/components/Profile/ProfileBio.jsx
+++ src/components/Profile/ProfileBio.jsx
@@ -1,4 +1,5 @@
 import Button from "@mui/material/Button";
+import UserStatistics from "./UserStatistics";
 
 const ProfileBio = ({ currentUser }) => {
   return (
@@ -8,7 +9,10 @@ const ProfileBio = ({ currentUser }) => {
       </div>
       <div>
         <h2 className="text-3xl text-white">{currentUser.username}</h2>
+        <UserStatistics />
         <p className="text-white text-lg">
+          {/* Needs to be replaced with currentUser.userBio once the Edit Profile function is ready.\
+          Leaving Lorem ipsum for testing purposes for now. */}
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </p>
---
+++ src/components/Profile/UserStatistics.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const UserStatistics = () => {
+  return (
+    <div className="text-white">
+      <div>
+        <span className="font-bold">200</span> Followers
+      </div>
+      <div>
+        <span className="font-bold">200</span> Collections
+      </div>
+      <div>
+        <span className="font-bold">50</span> Items
+      </div>
+    </div>
+  );
+};
+
+export default UserStatistics;
