a24e14b2ed4885cf013186fcbea58dbee0d81310
Johannes Chorzempa <jchorzempa@posteo.org>
2023-05-11 15:11:52 +0200

    send down a loading state

--- src/components/ImageUploader.jsx
+++ src/components/ImageUploader.jsx
@@ -2,20 +2,28 @@ import axios from "axios";
 
 const API_URL = "http://localhost:5005";
 
-const handleFileUpload = (setImageUrl, e) => {
+const handleFileUpload = (setImageUrl, e, setUploadingImage) => {
   const uploadData = new FormData();
 
   uploadData.append("imageUrl", e.target.files[0]);
+
+  setUploadingImage(true);
 
   axios
     .post(`${API_URL}/upload`, uploadData)
     .then((response) => {
       setImageUrl(response.data.fileUrl);
+      setUploadingImage(false);
+      console.log("File uploaded successfully: ", response)
     })
-    .catch((err) => console.log("Error while uploading the file: ", err));
-};
+    .catch((err) => {
+      console.log("Error while uploading the file: ", err);
+      setUploadingImage(false); // Set uploadingImage back to false in case of an error
+    });
+  }
 
-const ImageUploader = ({ setImageUrl, message }) => {
+
+const ImageUploader = ({ setImageUrl, message, setUploadingImage }) => {
   return (
     <div>
       <div className="row">
@@ -29,7 +37,7 @@ const ImageUploader = ({ setImageUrl, message }) => {
         <input
           type="file"
           id="image"
-          onChange={(e) => handleFileUpload(setImageUrl, e)}
+          onChange={(e) => handleFileUpload(setImageUrl, e, setUploadingImage)}
         />
       </div>
     </div>
