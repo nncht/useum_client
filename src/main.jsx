fb00fb62af07468ae09b4a81bb0b5c9cea7b8678
Johannes Chorzempa <jchorzempa@posteo.org>
2023-05-12 22:31:36 +0200

    Felt lazy and wrapped the whole app

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,17 @@ import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router} from 'react-router-dom'
 import { AuthProvider } from './context/auth.context.jsx'
+import { UserDataProvider } from './context/userData.context.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
+    <UserDataProvider>
       <AuthProvider>
         <App />
       </AuthProvider>
+      </UserDataProvider>
     </Router>
   </React.StrictMode>,
 )
