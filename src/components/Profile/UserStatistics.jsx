c807e553e9b91d9c70a9be63126f8a541cac5696
Johannes Chorzempa <jchorzempa@posteo.org>
2023-05-12 20:27:49 +0200

    Re-enabled user statistics cause they work now

--- src/components/Profile/UserStatistics.jsx
+++ src/components/Profile/UserStatistics.jsx
@@ -3,28 +3,30 @@ import { UserDataContext } from "../../context/userData.context";
 
 const UserStatistics = () => {
   const { userData } = useContext(UserDataContext);
-  console.log(userData);
 
-  return (
+  // Don't ask me why that line below makes it work magically...
+
+  if(userData.followers || userData.collections || userData.items) {
+    return (
     <div className="text-white flex">
-      {userData && (
+
         <>
           <div className="mr-4">
-            {/* <span className="font-bold">{userData.followers.length}</span>{" "} */}
+            <span className="font-bold">{userData.followers.length}</span>{" "}
             Followers
           </div>
           <div className="mr-4">
-            {/* <span className="font-bold">{userData.collections.length}</span>{" "} */}
+            <span className="font-bold">{userData.collections.length}</span>{" "}
             Collections
           </div>
           <div>
             {/* This doesn't work yet and the code might need to be altered, but I'll wait until Lukas is done updating the models. */}
-            {/* <span className="font-bold">{userData.items.length}</span> Items */}
+            <span className="font-bold">{userData.items.length}</span> Items
           </div>
         </>
-      )}
+
     </div>
-  );
+  );}
 };
 
 export default UserStatistics;
