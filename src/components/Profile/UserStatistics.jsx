59a77101531fbe8b55d15671a9a87faf11f70085
Nana Chuto <mail@nanachuto.com>
2023-05-12 17:52:43 +0200

    The problem is UserStatistics

--- src/components/Profile/UserStatistics.jsx
+++ src/components/Profile/UserStatistics.jsx
@@ -1,18 +1,17 @@
-
 const UserStatistics = ({ currentUser }) => {
   return (
     <div className="text-white flex">
       <div className="mr-4">
-        <span className="font-bold">{currentUser.followers.length}</span>{" "}
+        {/* <span className="font-bold">{currentUser.followers.length}</span>{" "} */}
         Followers
       </div>
       <div className="mr-4">
-        <span className="font-bold">{currentUser.collections.length}</span>{" "}
+        {/* <span className="font-bold">{currentUser.collections.length}</span>{" "} */}
         Collections
       </div>
       <div>
         {/* This doesn't work yet and the code might need to be altered, but I'll wait until Lukas is done updating the models. */}
-        <span className="font-bold">{currentUser.items.length}</span> Items
+        {/* <span className="font-bold">{currentUser.items.length}</span> Items */}
       </div>
     </div>
   );
