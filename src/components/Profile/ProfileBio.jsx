9ec9b508f72775dc362662455970c882931936e0
Nana Chuto <mail@nanachuto.com>
2023-05-13 15:38:44 +0200

    Correct margins

--- src/components/Profile/ProfileBio.jsx
+++ src/components/Profile/ProfileBio.jsx
@@ -14,6 +14,7 @@ const ProfileBio = () => {
   const { user } = useContext(AuthContext);
   const [currentUser, setCurrentUser] = useState(null);
 
+  // Request user object of currentUser (currently logged in user)
   useEffect(() => {
     if (user) {
       axios
@@ -31,25 +32,23 @@ const ProfileBio = () => {
     return (
       <div className="grid grid-auto-rows bg-slate-600 px-4 h-30 py-2">
         {/* Follow, Unfollow, Edit Profile buttons */}
-        <div className="text-right pt-34">
+
+        <div className="text-right my-3">
           {currentUser.username === userData.username ? (
-            <Link to={`/edit/${userData.username}`} className="m-2">
+            <Link to={`/edit/${userData.username}`}>
               <Button variant="contained">Edit Profile</Button>
             </Link>
           ) : currentUser.following.includes(userData._id) ? (
             // Link needs to be added after follow/unfollow routes have been written
-            <Button variant="outlined" className="m-2">
-              Unfollow
-            </Button>
+            <Button variant="outlined">Unfollow</Button>
           ) : (
             // Link needs to be added after follow/unfollow routes have been written
-            <Button variant="contained" className="m-2">
-              Follow
-            </Button>
+            <Button variant="contained">Follow</Button>
           )}
         </div>
+
         <div>
-          <div className="flex">
+          <div className="flex mt-2">
             {/* Username */}
             <h2 className="mr-4 self-end text-3xl text-white m-0">
               {userData.username}
