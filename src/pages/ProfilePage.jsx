af677fea634153523455dae0a9c0c29370c1fba2
Nana Chuto <mail@nanachuto.com>
2023-05-13 12:51:13 +0200

    Improve readability

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,16 +1,24 @@
 import { useContext, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
-import { UserDataContext } from "../context/userData.context";
 import axios from "axios";
-import Button from "@mui/material/Button";
-import { Grid } from "@mui/material";
+import { UserDataContext } from "../context/userData.context";
+
+// Custom components
 import ProfileHeader from "../components/Profile/ProfileHeader";
 import ProfileBio from "../components/Profile/ProfileBio";
 import ProfilePicture from "../components/Profile/ProfilePicture";
 import CollectionCard from "../components/Collections/CollectionCard";
 
+// MUI components
+import Button from "@mui/material/Button";
+import { Grid } from "@mui/material";
+import { comma } from "postcss/lib/list";
+
+// ----- End of imports
+
 const API_URL = "http://localhost:5005";
 
+// Fetch user object from username in URL
 const ProfilePage = () => {
   const { username } = useParams();
   const { userData, setUserData } = useContext(UserDataContext);
@@ -24,7 +32,9 @@ const ProfilePage = () => {
     fetchData();
   }, [username, setUserData]);
 
+  // RENDER PROFILE PAGE
   return (
+    // Profile header
     <div id="main-content">
       <div className="relative">
         <ProfileHeader />
@@ -37,8 +47,11 @@ const ProfilePage = () => {
         <ProfileBio />
       </div>
 
+      {/* Profile body */}
       <section className="px-4 pt-3 pb-20 bg-slate-300">
         <h4 className="text-2xl text-slate-600">Collections</h4>
+
+        {/* Render available collections */}
         <Grid container spacing={3}>
           {userData.collections && userData.collections ? (
             userData.collections.map((collection) => (
@@ -53,6 +66,7 @@ const ProfilePage = () => {
           )}
         </Grid>
 
+        {/* Create new collection button */}
         <nav className="my-4">
           <Link to="/create-collection" className="m-2">
             <Button variant="contained">New collection</Button>
