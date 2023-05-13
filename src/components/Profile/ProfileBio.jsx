6a7a82d624b3b356134dcf9ba356819b2e55eb69
Nana Chuto <mail@nanachuto.com>
2023-05-13 15:33:14 +0200

    Followers/following array is being sent now

--- src/components/Profile/ProfileBio.jsx
+++ src/components/Profile/ProfileBio.jsx
@@ -1,4 +1,5 @@
-import { useContext } from "react";
+import axios from "axios";
+import { useContext, useEffect, useState } from "react";
 import Button from "@mui/material/Button";
 import UserStatistics from "./UserStatistics";
 import CategoryTags from "./CategoryTags";
@@ -6,22 +7,38 @@ import { AuthContext } from "../../context/auth.context";
 import { UserDataContext } from "../../context/userData.context";
 import { Link } from "react-router-dom";
 
+const API_URL = "http://localhost:5005";
+
 const ProfileBio = () => {
   const { userData } = useContext(UserDataContext);
   const { user } = useContext(AuthContext);
+  const [currentUser, setCurrentUser] = useState(null);
 
-  console.log(user);
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(`${API_URL}/users/${user.username}`)
+        .then((res) => {
+          setCurrentUser(res.data);
+        })
+        .catch((err) => {
+          console.error(err);
+        });
+    }
+  }, [user]);
 
-  if (user) {
+  console.log(currentUser);
+
+  if (currentUser) {
     return (
       <div className="grid grid-auto-rows bg-slate-600 px-4 h-30 py-2">
         {/* Follow, Unfollow, Edit Profile buttons */}
         <div className="text-right pt-34">
-          {user.username === userData.username ? (
+          {currentUser.username === userData.username ? (
             <Link to={`/edit/${userData.username}`} className="m-2">
               <Button variant="contained">Edit Profile</Button>
             </Link>
-          ) : user.following.includes(userData._id) ? (
+          ) : currentUser.following.includes(userData._id) ? (
             // Link needs to be added after follow/unfollow routes have been written
             <Button variant="outlined" className="m-2">
               Unfollow
