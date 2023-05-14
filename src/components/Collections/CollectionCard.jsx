fda6322b6bf96623734835de09d57c07edd106d7
Nana Chuto <mail@nanachuto.com>
2023-05-14 12:24:04 +0200

    Remove links from collection card

--- src/components/Collections/CollectionCard.jsx
+++ src/components/Collections/CollectionCard.jsx
@@ -1,63 +1,55 @@
-import { Link } from "react-router-dom";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
 
 const CollectionCard = ({ collection }) => {
   return (
     <Card sx={{ maxWidth: 345 }}>
-      <CardMedia
-        sx={{ height: 200, filter: "brightness(90%)" }}
-        image={
-          collection.imageUrl === ""
-            ? "/images/default/default-collection.svg"
-            : collection.imageUrl
-        }
-        title={collection.name}
-      />
-      <CardContent>
-        <Typography
-          gutterBottom
-          variant="h5"
-          component="div"
-          sx={{
-            minHeight: "2.4rem",
-            maxHeight: "2.4rem",
-            overflow: "hidden",
-            textOverflow: "ellipsis",
-            whiteSpace: "nowrap",
-          }}
-        >
-          {collection.name}
-        </Typography>
+      <CardActionArea href={`/collections/${collection._id}`}>
+        <CardMedia
+          sx={{ height: 200, filter: "brightness(90%)" }}
+          image={
+            collection.imageUrl === ""
+              ? "/images/default/default-collection.svg"
+              : collection.imageUrl
+          }
+          title={collection.name}
+        />
+        <CardContent>
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="div"
+            sx={{
+              minHeight: "2.4rem",
+              maxHeight: "2.4rem",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              whiteSpace: "nowrap",
+            }}
+          >
+            {collection.name}
+          </Typography>
 
-        <Typography
-          variant="body2"
-          color="text.secondary"
-          sx={{
-            minHeight: "2.4rem",
-            maxHeight: "2.4rem",
-            overflow: "hidden",
-            textOverflow: "ellipsis",
-            display: "-webkit-box",
-            WebkitLineClamp: 2,
-            WebkitBoxOrient: "vertical",
-          }}
-        >
-          {collection.description}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        <Link to={`/collections/${collection._id}`} className="m-2">
-          <Button size="small">View</Button>
-        </Link>
-        <Link to="#" className="m-2">
-          <Button size="small">Save</Button>
-        </Link>
-      </CardActions>
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            sx={{
+              minHeight: "2.4rem",
+              maxHeight: "2.4rem",
+              overflow: "hidden",
+              textOverflow: "ellipsis",
+              display: "-webkit-box",
+              WebkitLineClamp: 2,
+              WebkitBoxOrient: "vertical",
+            }}
+          >
+            {collection.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
     </Card>
   );
 };
