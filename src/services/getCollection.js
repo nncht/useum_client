fb2a98b66b32a510d3add049d9550523a455dfc6
Johannes Chorzempa <jchorzempa@posteo.org>
2023-05-15 14:13:42 +0200

    Fix weird errors for missing collectionID in Itemc

--- src/services/getCollection.js
+++ src/services/getCollection.js
@@ -6,6 +6,8 @@ import API_URL from './apiConfig';
 
 const getCollection = (collectionId, setCollection) => {
 		const storedToken = localStorage.getItem('authToken');
+
+		if (collectionId !== undefined) {
 		axios
 			.get(`${API_URL}/collections/${collectionId}`, {
 				headers: {
@@ -18,6 +20,9 @@ const getCollection = (collectionId, setCollection) => {
 			.catch((error) => {
 				console.error(error);
 			});
+		} else {
+			console.warn('collectionId as ItemCard prop is undefined. That is okay in this instance, it still works.');
+		}
 	};
 
     export default getCollection;
