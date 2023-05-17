ffac6b33e78e039e535f0d3392f43481edd191bd
Nana Chuto <mail@nanachuto.com>
2023-05-17 13:21:03 +0200

    Fix profile width, add profile footer

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -11,6 +11,7 @@ import ProfileBio from "../components/Profile/ProfileBio";
 import ProfilePicture from "../components/Profile/ProfilePicture";
 import CollectionCard from "../components/Collections/CollectionCard";
 import SectionHeader from "../components/UI/SectionHeader";
+import ProfileFooter from "../components/Profile/ProfileFooter";
 
 // MUI components
 import Button from "@mui/material/Button";
@@ -40,48 +41,54 @@ const ProfilePage = () => {
   // RENDER PROFILE PAGE
   return (
     // Profile header
-    <div id="profile" className="mx-4 mt-4 shadow-sm">
-      <div className="relative">
-        <ProfileHeader />
-        <div className="absolute mt-[-80px] mx-4">
-          <ProfilePicture />
+    <div id="main-content" className="flex justify-center">
+      <div id="main-section" className="my-4 shadow-md">
+        <div className="relative">
+          <ProfileHeader />
+          <div className="absolute mt-[-80px] mx-4">
+            <ProfilePicture />
+          </div>
         </div>
-      </div>
 
-      <div>
-        <ProfileBio />
-      </div>
+        <div>
+          <ProfileBio />
+        </div>
 
-      {/* Profile body */}
-      <section className="px-4 pt-3 pb-20 bg-slate-200">
-        <SectionHeader title="Popular Collections" />
+        {/* Profile body */}
+        <section className="px-4 pt-3 pb-20 bg-slate-100">
+          <SectionHeader title="Popular Collections" />
 
-        {/* Render available collections */}
-        <Grid container spacing={3}>
-          {userData.collections && userData.collections ? (
-            userData.collections.map((collection) => (
-              <Grid item xs={12} sm={6} md={4} lg={3} key={collection._id}>
-                <CollectionCard key={collection._id} collection={collection} />
+          {/* Render available collections */}
+          <Grid container spacing={3}>
+            {userData.collections && userData.collections ? (
+              userData.collections.map((collection) => (
+                <Grid item xs={12} sm={6} md={4} lg={3} key={collection._id}>
+                  <CollectionCard
+                    key={collection._id}
+                    collection={collection}
+                  />
+                </Grid>
+              ))
+            ) : (
+              <Grid item xs={12} sm={6} md={4} lg={3}>
+                <p>No collections available</p>
               </Grid>
-            ))
-          ) : (
-            <Grid item xs={12} sm={6} md={4} lg={3}>
-              <p>No collections available</p>
-            </Grid>
-          )}
-        </Grid>
+            )}
+          </Grid>
 
-        {/* Create new collection button */}
-        {user && user.username === userData.username ? (
-          <nav className="my-4">
-            <Link to="/create-collection" className="m-2">
-              <Button variant="contained">New collection</Button>
-            </Link>
-          </nav>
-        ) : (
-          <div></div>
-        )}
-      </section>
+          {/* Create new collection button */}
+          {user && user.username === userData.username ? (
+            <nav className="my-4">
+              <Link to="/create-collection" className="m-2">
+                <Button variant="contained">New collection</Button>
+              </Link>
+            </nav>
+          ) : (
+            <div></div>
+          )}
+        </section>
+        <ProfileFooter />
+      </div>
     </div>
   );
 };
