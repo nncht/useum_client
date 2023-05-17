61c47f78c846af4720de289026e93dafd8574a3a
Nana Chuto <mail@nanachuto.com>
2023-05-17 15:05:31 +0200

    Import dependencies

--- src/components/Collections/CreateCollectionForm.jsx
+++ src/components/Collections/CreateCollectionForm.jsx
@@ -1,4 +1,18 @@
+import axios from "axios";
+import { useState, useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/auth.context";
+import API_URL from "../../services/apiConfig";
+
+// Custom components
+import ImageUploader from "../ImageUploader/ImageUploader";
+import SelectCategories from "../SelectCategories";
 import SectionHeader from "../UI/SectionHeader";
+
+// MUI imports
+import Button from "@mui/material/Button";
+
+// --- End of imports
 
 const CreateCollectionForm = () => {
   return (
