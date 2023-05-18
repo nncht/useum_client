d1425d3a8001cef3ffdfc913976ca2eb3839f4e9
Johannes Chorzempa <jchorzempa@posteo.org>
2023-05-18 21:45:35 +0200

    Update likebutton to include the right logic

--- src/components/Likes/LikeButton.jsx
+++ src/components/Likes/LikeButton.jsx
@@ -1,114 +1,103 @@
-import { AuthContext } from "../../context/auth.context";
-import { UserDataContext } from "../../context/userData.context";
-import { useContext, useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
-import API_URL from "../../services/apiConfig";
-import axios from "axios";
+import { AuthContext } from '../../context/auth.context';
+import { useContext, useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
+import API_URL from '../../services/apiConfig';
+import axios from 'axios';
 
 import Button from '@mui/material/Button';
 
-const BookmarkButton = ({id}) => {
+const LikeButton = ({ id }) => {
+	//id here either means collection or item id
 
-    const { user } = useContext(AuthContext);
-    const { userData, setUserData } = useContext(UserDataContext);
+	const { user } = useContext(AuthContext);
 
+	console.log(id);
 
+	const [currentUser, setCurrentUser] = useState(null);
 
-    console.log(id)
+	const [isLiked, setIsLiked] = useState(false);
+	const [likeList, setLikeList] = useState([]);
 
+	useEffect(() => {
+		if (user) {
+			axios
+				.get(`${API_URL}/users/${user.username}`)
+				.then((res) => {
+					setCurrentUser(res.data);
+				})
+				.catch((err) => {
+					console.error(err);
+				});
+		}
+	}, [user]);
 
+	//Check if User has already liked this item/collection
 
-    const [currentUser, setCurrentUser] = useState(null);
+	useEffect(() => {
+		if (currentUser && currentUser.likes.includes(id)) {
+			setIsLiked(true);
+		} else {
+			setIsLiked(false);
+		}
+	}, [currentUser, id]);
 
-    const [isBookmarked, setIsBookmarked] = useState(false);
-    const [bookmarkList, setBookmarkList] = useState([]);
+	//Like action
 
-    useEffect(() => {
-        if (user) {
-          axios
-            .get(`${API_URL}/users/${user.username}`)
-            .then((res) => {
-              setCurrentUser(res.data);
-            })
-            .catch((err) => {
-              console.error(err);
-            });
-        }
-      }, [user]);
+	const handleLike = async () => {
+		try {
+			const response = await axios.post(`${API_URL}/${currentUser._id}/like/${id}`);
+			setIsLiked(true);
+			setLikeList([...likeList, id]);
+			console.log(response.data);
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-      //Check if User has already bookmarked this item/collection
+	//Unlike action
 
-        useEffect(() => {
-            if (currentUser && currentUser.bookmarks.includes(id)) {
-                setIsBookmarked(true);
-            } else {
-                setIsBookmarked(false);
-            }
-        }, [currentUser, id]);
+	const handleUnlike = async () => {
+		try {
+			const response = await axios.post(`${API_URL}/${currentUser._id}/unlike/${id}`);
+			setIsLiked(false);
+			setLikeList([...likeList, id]);
+			console.log(response.data);
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-        //Bookmark action
+	return (
+		<div>
+			{isLiked ? (
+				<Button
+					key={Date.now()} // This magically forces the component to refresh
+					variant='contained'
+					className='unlike-btn'
+					onMouseOver={() => {
+						document.querySelector('.unlike-btn').textContent = 'Unlike';
+					}}
+					onMouseOut={() => {
+						document.querySelector('.unlike-btn').textContent = 'Like';
+					}}
+					onClick={handleUnlike}
+				>
+					Like
+				</Button>
+			) : (
+				<>
+					<Button
+						key={Date.now()} // This magically forces the component to refresh
+						variant='contained'
+						onClick={handleLike}
+					>
+						Unlike
+					</Button>
+					<p> You and {likeList.length - 1} others like this</p>
+				</>
+			)}
+		</div>
+	);
+};
 
-        const handleBookmark = async () => {
-            try {
-                const response = await axios.post(
-                    `${API_URL}/${currentUser._id}/bookmark/${id}`
-                );
-                setIsBookmarked(true);
-                setBookmarkList([...bookmarkList, id]);
-                console.log(response.data);
-
-            } catch (error) {
-                console.error(error);
-            }
-        };
-
-        //Unbookmark action
-
-        const handleUnbookmark = async () => {
-            try {
-                const response = await axios.post(
-                    `${API_URL}/${currentUser._id}/unbookmark/${id}`
-                );
-                setIsBookmarked(false);
-                setBookmarkList([...bookmarkList, id]);
-                console.log(response.data);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-
-
-
-  return (
-    <div>
-              {isBookmarked ? (
-                <Button
-                  key={Date.now()} // This magically forces the component to refresh
-                  variant="contained"
-                  className="unbookmark-btn"
-                  onMouseOver={() => {
-                    document.querySelector(".unbookmark-btn").textContent =
-                      "Unbookmark";
-                  }}
-                  onMouseOut={() => {
-                    document.querySelector(".unbookmark-btn").textContent =
-                      "Bookmarked";
-                  }}
-                  onClick={handleUnbookmark}
-                >
-                  Bookmarked
-                </Button>
-              ) : (
-                <Button
-                  key={Date.now()} // This magically forces the component to refresh
-                  variant="contained"
-                  onClick={handleBookmark}
-                >
-                  Bookmark
-                </Button>
-              )}
-            </div>
-  )
-}
-
-export default BookmarkButton
+export default LikeButton;
