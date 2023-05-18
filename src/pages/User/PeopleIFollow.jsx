57f88a548c7fec440dc6be0ab65fc53a5d070fab
Johannes Chorzempa <jchorzempa@posteo.org>
2023-05-18 14:21:29 +0200

    Base structure

--- src/pages/User/PeopleIFollow.jsx
+++ src/pages/User/PeopleIFollow.jsx
@@ -1,5 +1,18 @@
+import {useState, useEffect, useContext} from 'react'
+import { AuthContext } from '../../context/auth.context'
+import axios from 'axios'
+import API_URL from '../../services/apiConfig'
+import { Grid } from '@mui/material'
+
+
+
 
 const PeopleIFollow = () => {
+
+    const { user } = useContext(AuthContext);
+
+
+
   return (
     <div>PeopleIFollow</div>
   )
