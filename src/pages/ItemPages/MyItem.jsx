b30cc9cb963fa009c93fbab6b3dfef58e3144f7f
Nana Chuto <mail@nanachuto.com>
2023-05-20 00:25:43 +0200

    Update

--- src/pages/ItemPages/MyItem.jsx
+++ src/pages/ItemPages/MyItem.jsx
@@ -1,19 +1,23 @@
+import axios from "axios";
 import { useState, useEffect, useContext } from "react";
 import { useParams, Link } from "react-router-dom";
-import axios from "axios";
-import Button from "@mui/material/Button";
+import API_URL from "../../services/apiConfig";
 import { AuthContext } from "../../context/auth.context";
 import { setCollectionId } from "../../services/sharedDatastore";
 import { getCollectionId } from "../../services/sharedDatastore";
+
+// Custom components
 import AddItemToCollection from "../../components/Items/AddItemToCollection";
 import BookmarkButton from "../../components/Bookmarks/BookmarkButton";
-import API_URL from "../../services/apiConfig";
+
+// MUI imports
+import Button from "@mui/material/Button";
+
+// --- End of imports
 
 const MyItem = () => {
   const { itemId } = useParams();
-
   const { user } = useContext(AuthContext);
-
   const [item, setItem] = useState({});
   const [comments, setComments] = useState("");
   const collectionId = getCollectionId();
@@ -37,16 +41,20 @@ const MyItem = () => {
       {/* Displays the item details */}
 
       {item && (
-        <div id="main-content" className="bg-slate-300">
-          <section className="px-4 pt-3 pb-20 bg-slate-300">
-            <div>
-              <img src={item.imageUrl} width={250} height={350} alt="an item" />
+        <div id="main-content">
+          <div id="main-section" className="justify-center p-4">
+            <div className="p-4 bg-slate-50 rounded-md">
               <h4 className="text-2xl text-slate-600">{item.name}</h4>
+
+              <div className="py-4">
+                <img src={item.imageUrl} width={400} alt="an item" />
+              </div>
+              {/* Automatic description with OpenAI API */}
               <p>{item.description}</p>
 
               {item.categories && (
                 <div>
-                  <h4 className="text-2xl text-slate-600">Tags</h4>
+                  <h6 className="text-md text-slate-600">Categories</h6>
                   {item.categories.map((tag) => {
                     return <p key={tag._id}>{tag.category}</p>;
                   })}
@@ -62,7 +70,7 @@ const MyItem = () => {
                       <div key={comment._id}>
                         <h6>
                           <Link to={`/users/${comment.user.username}`}>
-                            {comment.user.username}{" "}
+                            {comment.user.username}
                           </Link>{" "}
                           says:
                         </h6>
@@ -73,23 +81,36 @@ const MyItem = () => {
                   })}
                 </div>
               )}
+
+              {/* Like button */}
+
+              <div className="flex flex-row gap-4 py-10">
+                <div>
+                  <Button
+                    variant="outlined"
+                    to={`/edit-item/${item._id}`}
+                    onClick={() => setCollectionId(collectionId)}
+                  >
+                    Edit Item
+                  </Button>
+                </div>
+                <div>
+                  <Button
+                    variant="contained"
+                    to={`/edit-item/${item._id}`}
+                    onClick={() => setCollectionId(collectionId)}
+                  >
+                    Like
+                  </Button>
+                </div>
+                <div>
+                  <BookmarkButton id={item._id} />
+                </div>
+              </div>
+
+              <AddItemToCollection itemId={item._id} />
             </div>
-
-            {/* Like button */}
-
-            <Link
-              to={`/edit-item/${item._id}`}
-              onClick={() => setCollectionId(collectionId)}
-            >
-              <Button>Edit Item</Button>
-            </Link>
-
-            <div>
-              <BookmarkButton id={item._id} />
-            </div>
-
-            <AddItemToCollection itemId={item._id} />
-          </section>
+          </div>
         </div>
       )}
     </>
