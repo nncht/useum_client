08df17ea997fdfd178528bfab26121d40b2503de
Nana Chuto <mail@nanachuto.com>
2023-05-19 19:29:58 +0200

    Search results sectioning done

--- src/pages/SearchResults.jsx
+++ src/pages/SearchResults.jsx
@@ -1,87 +1,117 @@
-import SectionHeader from '../components/UI/SectionHeader';
-import { useParams, useSearchParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import axios from 'axios';
-import API_URL from '../services/apiConfig';
-import { Grid } from '@mui/material';
-import ItemCard from '../components/Items/ItemCard';
-import CollectionCard from '../components/Collections/CollectionCard';
+import axios from "axios";
+import { useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+// Custom components
+import ItemCard from "../components/Items/ItemCard";
+import SectionHeader from "../components/UI/SectionHeader";
+import CollectionCard from "../components/Collections/CollectionCard";
+import API_URL from "../services/apiConfig";
+
+// MUI components
+import { Grid } from "@mui/material";
+
+// --- End of imports
 
 const SearchResults = () => {
-	const [searchParams, setSearchParams] = useSearchParams();
-	const [usersFound, setUsersFound] = useState([]);
-	const [itemsFound, setItemsFound] = useState([]);
-	const [collectionsFound, setCollectionsFound] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [usersFound, setUsersFound] = useState([]);
+  const [itemsFound, setItemsFound] = useState([]);
+  const [collectionsFound, setCollectionsFound] = useState([]);
 
-	const searchTerm = searchParams.get('q');
+  const searchTerm = searchParams.get("q");
 
-	useEffect(() => {
-		if (searchTerm) {
-			axios
-				.get(`${API_URL}/search?search=${searchTerm}`)
-				.then((res) => {
-					console.log(res.data);
-					setUsersFound(res.data.users);
-					setItemsFound(res.data.items);
-					setCollectionsFound(res.data.collections);
-				})
-				.catch((err) => {
-					console.log(err);
-				});
-		}
-	}, [searchTerm]);
+  useEffect(() => {
+    if (searchTerm) {
+      axios
+        .get(`${API_URL}/search?search=${searchTerm}`)
+        .then((res) => {
+          console.log(res.data);
+          setUsersFound(res.data.users);
+          setItemsFound(res.data.items);
+          setCollectionsFound(res.data.collections);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [searchTerm]);
 
-	return (
-		<div id='main-content'>
-			<section className='px-4 pt-3 pb-20 bg-slate-300'>
-				<SectionHeader title='Search Results' />
+  // -----------------
+  // RENDER UI
+  // -----------------
 
-				<SectionHeader title='Users' />
-				<Grid container spacing={3}>
-					{usersFound.length ? (
-						usersFound.map((user) => (
-							<Grid item xs={12} sm={6} md={4} lg={3} key={user._id}>
-								<div key={user._id}>
-									<img src={user.imageUrl} alt='' />
-									<p>{user.username}</p>
-								</div>
-							</Grid>
-						))
-					) : (
-						<p>No users found</p>
-					)}
-				</Grid>
+  return (
+    <>
+      <section id="main-content" className="bg-slate-100">
+        <div id="main-section" className="p-4">
+          <SectionHeader title="Search Results" />
 
-				<Grid container spacing={3}>
-					<SectionHeader title='Items' />
+          {/* Users */}
+          <SectionHeader title="Users" />
+          <Grid container spacing={3}>
+            {usersFound.length ? (
+              usersFound.map((user) => (
+                <Grid item xs={12} sm={6} md={4} lg={3} key={user._id}>
+                  <div key={user._id}>
+                    <img src={user.imageUrl} alt="" />
+                    <p>{user.username}</p>
+                  </div>
+                </Grid>
+              ))
+            ) : (
+              <div className="p-4">
+                <p>Could not find any matching users</p>
+              </div>
+            )}
+          </Grid>
+        </div>
+      </section>
 
-					{itemsFound.length ? (
-						itemsFound.map((item) => (
-							<Grid item xs={12} sm={6} md={4} lg={3} key={item._id}>
-								<ItemCard key={item._id} item={item} />
-							</Grid>
-						))
-					) : (
-						<p>No items found</p>
-					)}
-				</Grid>
+      {/* Items */}
+      <section id="main-content" className="bg-slate-200">
+        <div id="main-section" className="p-4">
+          <SectionHeader title="Items" />
+          <Grid container spacing={3}>
+            {itemsFound.length ? (
+              itemsFound.map((item) => (
+                <Grid item xs={12} sm={6} md={4} lg={3} key={item._id}>
+                  <ItemCard key={item._id} item={item} />
+                </Grid>
+              ))
+            ) : (
+              <div className="p-4">
+                <p>Could not find any matching items</p>
+              </div>
+            )}
+          </Grid>
+        </div>
+      </section>
 
-				<Grid container spacing={3}>
-					<SectionHeader title='Collections' />
-
-					{collectionsFound.length ? (
-						collectionsFound.map((collection) => (
-							<Grid item xs={12} sm={6} md={4} lg={3} key={collection._id}>
-								<CollectionCard key={collection._id} collection={collection} />
-							</Grid>
-						))
-					) : (
-						<p>No collections found</p>
-					)}
-				</Grid>
-			</section>
-		</div>
-	);
+      {/* Collections */}
+      <section id="main-content" className="bg-slate-100">
+        <div id="main-section" className="p-4">
+          <SectionHeader title="Collections" />
+          <Grid container spacing={3}>
+            {collectionsFound.length ? (
+              collectionsFound.map((collection) => (
+                <Grid item xs={12} sm={6} md={4} lg={3} key={collection._id}>
+                  <CollectionCard
+                    key={collection._id}
+                    collection={collection}
+                  />
+                </Grid>
+              ))
+            ) : (
+              <div className="p-4">
+                <p>Could not find any matching collections</p>
+              </div>
+            )}
+          </Grid>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default SearchResults;
