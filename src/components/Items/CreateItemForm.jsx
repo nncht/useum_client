45a7f04f8c19b686b70b3d67d73d4ef0a98c504c
Nana Chuto <mail@nanachuto.com>
2023-05-20 19:35:57 +0200

    Added create item info

--- src/components/Items/CreateItemForm.jsx
+++ src/components/Items/CreateItemForm.jsx
@@ -11,6 +11,7 @@ import SelectCategories from "../SelectCategories";
 
 // MUI imports
 import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 // --- End of imports
 
@@ -28,7 +29,7 @@ const CreateitemForm = ({ target, idObject, forCollection }) => {
   const [uploadingImage, setUploadingImage] = useState(false);
 
   const storedToken = localStorage.getItem("authToken");
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -37,17 +38,23 @@ const CreateitemForm = ({ target, idObject, forCollection }) => {
       organization: import.meta.env.VITE_APP_OPENAI_ORGANIZATION_KEY,
       apiKey: import.meta.env.VITE_APP_OPENAI_API_KEY,
     });
-  
-    delete configuration.baseOptions.headers['User-Agent'];
+
+    delete configuration.baseOptions.headers["User-Agent"];
     const openai = new OpenAIApi(configuration);
-  
+
     const createChatCompletion = async (name) => {
       const completion = await openai.createChatCompletion({
-      model: "gpt-3.5-turbo",
-      messages: [
-        { role: "system", content: `You are creating an item named "${name}"` },
-        { role: "user", content: `Describe the item "${name}" in detail with at least 40 words` },
-      ]
+        model: "gpt-3.5-turbo",
+        messages: [
+          {
+            role: "system",
+            content: `You are creating an item named "${name}"`,
+          },
+          {
+            role: "user",
+            content: `Describe the item "${name}" in detail with at least 40 words`,
+          },
+        ],
       });
       console.log(completion.data.choices[0].message);
       const description = completion.data.choices[0].message.content;
@@ -55,7 +62,7 @@ const CreateitemForm = ({ target, idObject, forCollection }) => {
     };
 
     //something wrong with headers
-  
+
     const description = await createChatCompletion(name);
 
     // Uploading cover images is optional
@@ -169,6 +176,14 @@ const CreateitemForm = ({ target, idObject, forCollection }) => {
             <Button variant="contained" type="submit" className="text-xl mt-3">
               Add item
             </Button>
+          </div>
+          <div id="main-section" className="p-4">
+            <Typography variant="h6" sx={{ color: "red" }}>
+              Creating new items works, it just takes a long time before the
+              process is finished due to auto-generated descriptions taking a
+              while. We still need to add loading spinners, until then give it a
+              little time before you'll see the created item.
+            </Typography>
           </div>
         </form>
 
